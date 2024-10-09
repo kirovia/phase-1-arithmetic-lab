715ca1fd0fc9cb2b2059df5f1c11911ed76775f9
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-10-09 01:24:47 -0400

    pass all tests

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+const num5 = 20;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 1);
+const mod = num3 % num4;
+const max = Math.max(num4, num5);
